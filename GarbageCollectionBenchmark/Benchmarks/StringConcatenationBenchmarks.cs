398ab57830d7e7be6f348db44bb4b8f2fe42d077
Giorgi Kobaidze <george.kobaidze.14@gmail.com>
2022-10-09 20:00:02 +0400

    Add string concatenation example

---
+++ GarbageCollectionBenchmark/Benchmarks/StringConcatenationBenchmarks.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using GarbageCollectionBenchmark.Models;
+
+namespace GarbageCollectionBenchmark.Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class StringConcatenationBenchmarks
+{
+    private static readonly StringConcatenation StringConcatenation = new StringConcatenation();
+
+    [Benchmark]
+    public void NonOptimalConcat() => StringConcatenation.NonOptimalConcat();
+    
+    [Benchmark]
+    public void OptimalConcat() => StringConcatenation.OptimizedConcat();
+}
---
+++ GarbageCollectionBenchmark/Models/StringConcatenation.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace GarbageCollectionBenchmark.Models;
+
+public class StringConcatenation
+{
+    public string NonOptimalConcat()
+    {
+        var builder = new StringBuilder();
+        for (var i = 0; i < 10000; i++)
+            builder.Append(i + "KB");
+
+        return builder.ToString();
+    }
+
+    public string OptimizedConcat()
+    {
+        var builder = new StringBuilder();
+        for (var i = 0; i < 10000; i++)
+        {
+            builder.Append(i);
+            builder.Append("KB");
+        }
+
+        return builder.ToString();
+    }
+}
